40c7e562e4e007fc3a4d72b06d996947ec466f0a
sameer <sameer.chaudhari@wohlig.com>
2023-01-13 18:05:18 +0530

    up

--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
+    
+    <v-toolbar color="cyan-lighten-1">
+      <v-btn variant="text" icon="mdi-menu"></v-btn>
+      <v-toolbar-title>Instagram</v-toolbar-title>
+      <nuxt-link class="edit" to="/"><v-btn>Home</v-btn></nuxt-link>
+      <nuxt-link class="edit" to="/home"><v-btn>User</v-btn></nuxt-link>
+      <nuxt-link class="edit" to="/about"><v-btn>About</v-btn></nuxt-link>
+      <!-- <v-text-field placeholder="Search ..." v-model="search"></v-text-field> -->
+    </v-toolbar>
     <h1>About</h1>
-    <h2>{{  }}</h2>
-    <pre>{{data}}</pre>
+    <!-- <h2>{{ testdata }}</h2> -->
+    <pre v-if="responseData && responseData.length">{{ responseData  }}</pre>
+    <pre> </pre>
   </div>
 </template>
 
 <script setup>
 var myHeaders = new Headers();
-var testdata = "user";
+// var testdata = "user";
+// var user = "i am a user";
 myHeaders.append("X-RapidAPI-Host", "instagram47.p.rapidapi.com");
 myHeaders.append(
   "X-RapidAPI-Key",
@@ -21,21 +32,23 @@ var requestOptions = {
   redirect: "follow",
 };
 
-const { response: data } = await useAsyncData("response", () => {
-  return new Promise((resolve, reject) => {
-    fetch(
-      "https://ac17bbe4-d0b1-47a1-8f66-0c6966a510e8.mock.pstmn.io/search?search=irtrtrtrtrtrtrtrt",
-      requestOptions
-    )
-      .then((response) => response.text())
-      .then((result) => {
-        resolve(result);
-        console.log(result);
-      })
-      .catch((error) => console.log("error", error));
-  }); 
+
+let responseData = ref([]);
+
+onMounted(async () => {
+  await fetch(
+    "https://ac17bbe4-d0b1-47a1-8f66-0c6966a510e8.mock.pstmn.io/search?search=irtrtrtrtrtrtrtrt",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      if (result && result.body && result.body.users) {
+        responseData = result.body.users;
+        console.log(responseData);
+      }
+    })
+    .catch((error) => console.log("error", error));
 });
-// console.log(response);
 </script>
 
 <style></style>
